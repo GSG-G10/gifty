f8d6f7040cb0a44ec5b20530129a8732b3b2bc1a
Raghad-Mezied <rmezied3@gmail.com>
2021-10-03 12:02:22 +0300

    edit the getProduct route to validate the product id

--- server/controllers/getProduct.js
+++ server/controllers/getProduct.js
@@ -1,6 +1,12 @@
 const getProductQuery = require('../database/queries');
 
-module.exports = (req, res) => {
+module.exports = (req, res, next) => {
   const { productId } = req.params;
-  getProductQuery(productId).then((data) => res.json(data.rows));
+  if (productId > 0) {
+    getProductQuery(productId)
+      .then((data) => res.json(data.rows))
+      .catch((err) => next(err));
+  } else {
+    res.status(400).json({});
+  }
 };
