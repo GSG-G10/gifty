97f04966d33c59808142317e8f824004b0c6d65c
Ahmad-Omar <aw.omar7@gmail.com>
2021-10-03 15:17:05 +0300

    create delete product route Relates #16

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
-const getProduct = require('../controllers');
+const { getProduct } = require('../controllers');
+const { deleteProduct } = require('../controllers');
 
 router.get('/product/:productId', getProduct);
-
+router.delete('/deletePorduct/:productId', deleteProduct);
 module.exports = router;
