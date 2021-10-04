9828ed8756a971b0872a5317ae2a1301fd679c5c
umar <umars.salah@gmail.com>
2021-10-05 00:04:03 +0300

    edit Slider Component

--- client/src/components/slider/index.jsx
+++ client/src/components/slider/index.jsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
 import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 
@@ -14,18 +9,22 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
+    id:1,
     imgPath:
       'https://images.unsplash.com/photo-1602566178436-8cf72756f4cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
   },
   {
+    id:2,
     imgPath:
       'https://images.unsplash.com/photo-1578704694513-08946e996642?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
   },
   {
+    id:3,
     imgPath:
       'https://images.unsplash.com/photo-1519218470957-62c7c83c36b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
   },
   {
+    id:4,
     imgPath:
       'https://images.unsplash.com/photo-1577217534079-41d6bb68ac50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
   },
@@ -62,7 +61,6 @@ export const Slider = () => {
           bgcolor: 'background.default',
         }}
       >
-        <Typography>{images[activeStep].label}</Typography>
       </Paper>
       <AutoPlaySwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
@@ -71,7 +69,7 @@ export const Slider = () => {
         enableMouseEvents
       >
         {images.map((step, index) => (
-          <div key={step.label}>
+          <div key={step.id}>
             {Math.abs(activeStep - index) <= 2 ? (
               <Box
                 component="img"
@@ -79,46 +77,17 @@ export const Slider = () => {
                   display:'flex',
                   alignItems:'center',
                   justifyContent: 'center',
-                  height:"70vh",
+                  height:"86vh",
                   overflow: 'hidden',
                   width: '100%',
                 }}
                 src={step.imgPath}
-                alt={step.label}
+                alt="Cover"
               />
             ) : null}
           </div>
         ))}
       </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
     </Box>
   );
 }
