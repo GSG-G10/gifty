cf171a126704d42a8fa9b83f2b7c1912a83248d4
Ahmad-Omar <aw.omar7@gmail.com>
2021-10-05 16:59:48 +0300

    fix landingPage import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-import { Landing } from "./pages/landing";
+import Landing from "./pages/landing";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>NAV</h1>
       <Landing />
     </div>
   );
