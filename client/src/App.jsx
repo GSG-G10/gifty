9b10784ca96a2c28e7c2badf4f0845840b41e967
ameera abedalnaser <ameeraabed92@gmail.com>
2021-10-06 20:37:25 +0300

    solve-conflict related #20

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
 import './App.css';
 import Footer from './components/common/Footer';
-=======
->>>>>>> 8fa3a6e737b71b97ccea2215656a983ec8831fb2
 import Landing from "./pages/landing";
 import TabComponent from "./components/Tab";
-import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Landing />
-      <Footer /> 
-=======
-      {/* <Landing /> */}
+      
       <TabComponent />
->>>>>>> 8fa3a6e737b71b97ccea2215656a983ec8831fb2
+      <Footer /> 
     </div>
   );
 }
