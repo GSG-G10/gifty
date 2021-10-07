40114e558354e1ab9c0d5e404ffe26e58d7ef24d
Ahmad-Omar <aw.omar7@gmail.com>
2021-10-07 18:45:18 +0300

    footer component Relates #20

---
+++ client/src/components/common/Footer/index.jsx
@@ -0,0 +1,21 @@
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import logo from '../../../assets/gifty.png';
+import './style.css';
+
+const Footer = () => (
+    <footer className="footer">
+        <div className="container">
+          <img src={logo} alt="logo img" className="logo"/>
+          <nav className="nav">
+              <FacebookOutlinedIcon />
+              <InstagramIcon />
+              <TwitterIcon />
+              <WhatsAppIcon />
+          </nav>
+      </div>
+    </footer>
+);
+export default Footer;
