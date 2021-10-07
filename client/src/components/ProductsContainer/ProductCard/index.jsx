2a654de74bb5f068f7b76cd5bd31c4275d61c7a8
Ahmad-Omar <aw.omar7@gmail.com>
2021-10-07 16:44:45 +0300

    handle add to cart functionality

--- client/src/components/ProductsContainer/ProductCard/index.jsx
+++ client/src/components/ProductsContainer/ProductCard/index.jsx
@@ -1,3 +1,4 @@
+import { useState, forwardRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -5,6 +6,12 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActions } from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import MuiAlert from '@mui/material/Alert';
+
+import axios from 'axios';
 
 import './style.css';
 
@@ -12,8 +19,47 @@ function ProductCard({
   productID, productImage, productName, productPrice,
 }) {
   const history = useHistory();
+  const [errorMsg, setErrorMsg] = useState('');
+  const [addedMsg, setAddedMsg] = useState('');
+  const [added, setAdded] = useState(false);
+  const [sthWrong, setSthWrong] = useState(false);
+  const Alert = forwardRef((props, ref) => <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />);
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setAdded(false);
+  };
+  const action = (
+    <>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+  );
   const handleClick = () => {
     history.push(`/product/${productID}`);
+  };
+
+  const handleAddToCart = () => {
+    axios.post('/addToCart', {
+      productId: productID, quantity: 1,
+    })
+      .then((response) => response.data)
+      .then((result) => {
+        setAddedMsg(result.msg);
+        setAdded(true);
+      })
+      .catch(() => {
+        setErrorMsg('Something wrong!');
+        setSthWrong('Something wrong!');
+      });
   };
   return (
       <Card variant="outlined" style={{ cursor: 'pointer' }}>
@@ -54,10 +100,24 @@ function ProductCard({
               $ {productPrice}
             </Typography>
           </CardActions>
-          <Button size="small">
+          <Button size="small" onClick={handleAddToCart}>
             <AddShoppingCartIcon style={{ color: '#000' }} />
           </Button>
         </CardContent>
+        {!errorMsg
+          ? <Snackbar
+         open={added}
+         autoHideDuration={6000}
+         onClose={handleClose}
+         action={action}
+       ><Alert severity="success">{addedMsg}</Alert></Snackbar> : <Snackbar
+       open={sthWrong}
+       autoHideDuration={6000}
+       onClose={handleClose}
+       action={action}
+     ><Alert severity="error">Something Wrong!</Alert></Snackbar>
+      }
+
       </Card>
   );
 }
