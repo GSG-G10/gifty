632a039664976f9fdaeca9ee3a3a52486e197270
Raghad-Mezied <rmezied3@gmail.com>
2021-10-07 22:54:53 +0300

    solve conflict

--- server/controllers/getUserCart.js
+++ server/controllers/getUserCart.js
@@ -2,10 +2,7 @@ const { getUserCartQuery } = require('../database/queries');
 
 module.exports = (req, res, next) => {
   const { userId } = req;
-<<<<<<< HEAD
 
-=======
->>>>>>> 102cfb0935d252ae42160a1e8ff3efeb09b3f2d2
   getUserCartQuery(userId)
     .then(({ rows }) => res.json({ data: rows }))
     .catch((err) => next(err));
